1a10e20c279e5f934b14b0d5a18f0547f1292047
CharlieUofT <chenlei.hu@mail.utoronto.ca>
2017-08-01 14:26:29 +0800

    Add more styles for QR code decoding page.

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,7 @@ const router = express.Router();
  * User scan QR code convert the qr code to a code copy page
  */
 router.get('/convert/qrcode', function (req, res, next) {
+    res.locals.title = "Bind Card";
     res.locals.serial = req.query['serial'];
     res.render('serial_number.jade');
 });
